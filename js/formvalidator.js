7645c82b5ca8f4df6c756d4f3533edf6759b444f
rahulraman989 <rahulraman989@gmail.com>
2015-03-31 22:55:55 +0800

    form validator and modimified form action/onsub

---
+++ js/formvalidator.js
@@ -0,0 +1,17 @@
+<script> 
+function validateForm() { 
+	var full_name = document.getElementById('full_name').value; 
+	var email = document.getElementById('email_id').value; 
+	var newsletter_subscriptionchecked = document.getElementById('newsletter_subscription').checked; 
+	
+	if ((full_name !== null || full_name !== "") && (email !== null || email !== "") && 
+		(newsletter_subscriptionchecked))
+		{
+			alert ("Please fill in all the mandatory fields"); 
+			return false;
+		}
+		else {
+			return true;
+		}
+		} 
+</script>
