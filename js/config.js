fcc907af54d16398adfb003d291b30b8161c727c
rahulraman989 <rahulraman989@gmail.com>
2015-12-08 14:33:27 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+var _seg = {
+      "priority" : "pr",
+      "business-banking-sme" : "bs",
+      "saadiq" : "sq",
+      "rmb" : "rm",
+      "en" : "pv",
+	    "nri" : "ni",
+	    "employee-banking": "em",
+	    "staff": "st"
+    };
